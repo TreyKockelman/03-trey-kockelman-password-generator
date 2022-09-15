dfcf5e10b9df5a2ca30037dba28810bdfafaa927
TreyKockelman <kockelmant@gmail.com>
2022-09-15 16:24:59 -0500

    finished the functions for preferences

--- script.js
+++ script.js
@@ -4,44 +4,49 @@ var generateBtn = document.querySelector("#generate");
 // Global variables
 var numOfCharacters;
 var useLowercaseLetters;
-var allLowercaseLetters; ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z"];
+var allLowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z"];
 var useUppercaseLetters;
-var allUppercaseLetters; ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var allUppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers;
 var allNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialCharacters;
+var allSpecialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", ">", "<", ",", ".", "/", "=", "_", "`"];
+var min = 8;
+var max = 128;
 var random = Math.floor(Math.random() * (max - min + 1) +min);
-var passwordRandomString = ""
+var passwordRandomString = "";
 
-// Asks user how many characters they want to generate in their password?
+// Asks user how many characters they want to generate in their password
 function howManyCharacters(){
   numOfCharacters = parseInt( prompt("How many characters?") );
   console.log(numOfCharacters);
 }
 
-// Asks user if they want lower case letters?
+// Asks user if they want lower case letters
 function useLowercase(){
   useLowercaseLetters = confirm("Do you want lowercase letters in the password?");
   console.log(useLowercaseLetters)
 }
 
-// Asks user if they want upper case letters?
+// Asks user if they want upper case letters
 function useUppercase(){
   useUppercaseLetters = confirm("Do you want uppercase letters in the password?");
   console.log(useUppercaseLetters)
 }
 
-// Asks user if they want to use numbers?
+// Asks user if they want to use numbers
 function useNumbers(){
   numbers = confirm("Do you want numbers in the password?");
   console.log(numbers)
 }
 
-// Asks user if they want to use special characters?
+// Asks user if they want to use special characters
 function useSpecialCharacters(){
   specialCharacters = confirm("Do you want special characters in the password?");
   console.log(specialCharacters)
 }
+
+// Need to set max password length and need to set preferences from user inputs
 
 // Generates a random password string from user inputs
 
@@ -52,11 +57,10 @@ function generatePassword() {
   useNumbers();
   useSpecialCharacters();
 
-
-
-  return passwordRandomString();
+  return passwordRandomString;
 }
-// Write password to the #password input
+
+// Assignment Code: Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
