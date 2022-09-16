3f6a0c59339fc4535e433cdfd019e2b887333a75
TreyKockelman <kockelmant@gmail.com>
2022-09-15 21:15:09 -0500

    added for loop

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ var generateBtn = document.querySelector("#generate");
 // Global variables
 var numOfCharacters;
 var useLowercaseLetters;
-var allLowercaseLetters = ["abcdefghijklmnopqrstuvwxyz"];
+var allLowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 var useUppercaseLetters;
-var allUppercaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var allUppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers;
-var allNumbers = ["1234567890"];
+var allNumbers = "1234567890"
 var specialCharacters;
-var allSpecialCharacters = ["~!@#$%^&*()-+?><,./=_`"];
-var min = 8;
-var max = 128;
+var allSpecialCharacters = "~!@#$%^&*()-+?><,./=_`"
+var min;
+var max;
 var userPasswordPreferencesString = "";
+var passwordPreferences = [allLowercaseLetters, allUppercaseLetters, allNumbers, allSpecialCharacters]
 var passwordElements = [useLowercaseLetters, useUppercaseLetters, numbers, specialCharacters];
-var random = Math.floor(Math.random() * (max - min + 1) +min);
 var passwordRandomString = "";
 
 // Asks user how many characters they want to generate in their password
@@ -49,35 +49,40 @@ function useSpecialCharacters(){
 }
 
 // Need to set max password length and need to set preferences from user inputs, include true statements and exclude false statements from above, need loop for 4 functions after amount of characters, want to put all arrays that have a true statement as a single string. is because userPasswordPreferencesString is a global variable and the for/if statement it is a child? //
-// for (var i = 0; i < passwordElements.length; i++){
 
 // Generates a random password string from user inputs
 
 function generatePassword() {
+  passwordRandomString = "";
+  userPasswordPreferencesString = "";
   howManyCharacters();
   useLowercase();
   useUppercase();
   useNumbers();
   useSpecialCharacters();
-  var userPasswordPreferencesString
-  if (useLowercaseLetters === true) {
-    userPasswordPreferencesString =+ allLowercaseLetters;
-    console.log(allLowercaseLetters);
+  if (useLowercaseLetters) {
+    userPasswordPreferencesString += allLowercaseLetters;
+    console.log(userPasswordPreferencesString);
   }
-  if (useUppercaseLetters === true) {
-    userPasswordPreferencesString =+ allLowercaseLetters;
-    console.log(allUppercaseLetters);
+  if (useUppercaseLetters) {
+    userPasswordPreferencesString += allUppercaseLetters;
+    console.log(userPasswordPreferencesString);
   }
-  if (numbers === true) {
-    userPasswordPreferencesString =+ allLowercaseLetters;
-    console.log(allNumbers);
+  if (numbers) {
+    userPasswordPreferencesString += allNumbers;
+    console.log(userPasswordPreferencesString);
   }
-  if (specialCharacters === true) {
-    userPasswordPreferencesString =+ allLowercaseLetters;
-    console.log(allSpecialCharacters);
+  if (specialCharacters) {
+    userPasswordPreferencesString += allSpecialCharacters;
+    console.log(userPasswordPreferencesString);
   }
-  console.log(userPasswordPreferencesString);
-  return passwordRandomString;
+  max = userPasswordPreferencesString.length
+  min = 0
+  for (var i = 0; i < numOfCharacters; i++) {
+    var random = Math.floor(Math.random() * (max - min + 1) +min);
+    passwordRandomString += userPasswordPreferencesString[random];
+  }
+return passwordRandomString;
 }
 
 // Assignment Code: Write password to the #password input
